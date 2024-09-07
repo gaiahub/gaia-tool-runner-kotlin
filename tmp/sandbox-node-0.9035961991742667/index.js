6111bddc6a1bedb36f7020ec919e40f0906e542f
Loterio <natanloterio@gmail.com>
2024-09-07 20:27:12 +0100

    update .gitignore

--- tmp/sandbox-node-0.9035961991742667/index.js
+++
@@ -1,7 +0,0 @@
---------------------------YCrTqxJlDxcvh8fjtOzj5d
-Content-Disposition: form-data; name="file"; filename="program.js"
-Content-Type: application/octet-stream
-
-console.log(\'test\')
-
---------------------------YCrTqxJlDxcvh8fjtOzj5d--
