8806a3c5ecc82e2ae846827ffdf747e84c5fcf9b
Natan <ext.natan.loterio@farfetch.com>
2024-09-09 09:50:40 +0100

    Rebase with main

--- docker/tool-runner-controller/bootstart.sh
+++ docker/tool-runner-controller/bootstart.sh
@@ -16,4 +16,5 @@ echo "Starting the Gaia Tool Runner"
 
 # Schedule the task to run in 1 minute
 echo java -jar /app/gaiaRunner.jar --port $PORT --jdbc "$JDBC_CONNECTION_STRING"
-java -jar /app/gaiaRunner.jar --port $PORT --jdbc "$JDBC_CONNECTION_STRING"
+# java -jar /app/gaiaRunner.jar --port $PORT --jdbc "$JDBC_CONNECTION_STRING"
+nohup java -jar /app/gaiaRunner.jar --port $PORT --jdbc "$JDBC_CONNECTION_STRING" 2>>/dev/null >>/dev/null&
